552d809aa4272b6d8d929a7e65a8e8ede248f887
sidderick <davidsiddens@hotmail.co.uk>
2022-03-29 12:49:56 +0100

    Add files via upload

---
+++ rpg-game/src/main/java/com/gamefiles/rpggame/RpgGameApplication.java
@@ -0,0 +1,13 @@
+package com.gamefiles.rpggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgGameApplication.class, args);
+	}
+
+}
---
+++ rpg-game/src/test/java/com/gamefiles/rpggame/RpgGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamefiles.rpggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
