2cf7359d2732c7435bb6c633dd7ba9cf9b41ab18
ajeglon <61421392+ajeglon@users.noreply.github.com>
2022-04-01 08:33:29 +0100

    Table ID fix

--- rpg-game/src/main/java/com/gamefiles/rpggame/model/enemy/Enemy.java
+++ rpg-game/src/main/java/com/gamefiles/rpggame/model/enemy/Enemy.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 public class Enemy {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private int health;
--- rpg-game/src/main/java/com/gamefiles/rpggame/model/item/Item.java
+++ rpg-game/src/main/java/com/gamefiles/rpggame/model/item/Item.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Item{
 
         @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Integer id;
         private String name;
         private int healthValue;
